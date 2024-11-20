d9a9ce7f0d20153b0225ced28759d07b7e2062e9
Carolina Sebastião <angelacarolina3456@gmail.com>
2024-11-20 10:54:13 +0000

    criar Navbar

--- frontoffice/KITTYBLOG/src/App.jsx
+++ frontoffice/KITTYBLOG/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Navbar from './NavBar/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+     <Navbar></Navbar>
     </>
   )
 }
---
+++ frontoffice/KITTYBLOG/src/NavBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+function Navbar(){
+    return(
+    <>
+        <h1>jzkdxjdbc</h1>
+    </>
+    )
+}
+
+export default Navbar
