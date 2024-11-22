4e42cc9c34e40300d26c86339b5e15d3b45f1a3b
Carolina Sebastião <angelacarolina3456@gmail.com>
2024-11-22 11:22:32 +0000

    LOGIN PAGE

--- frontoffice/KITTYBLOG/src/main.jsx
+++ frontoffice/KITTYBLOG/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import App from './APP/App'
+import App from './App'
+import Navbar from './NavBar/Navbar'
+import Login from './Login/login'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
