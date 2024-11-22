d073c91ff9d44ba5bf526d4be067cf66bec9bdb6
Carolina Sebastião <angelacarolina3456@gmail.com>
2024-11-22 11:29:13 +0000

    LOGIN

---
+++ frontoffice/KITTYBLOG/src/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Login.css"
+export default function Login()
+{
+return(
+    <>
+    <div id="boxLogin">
+        <div id="divLogin">
+            <h1 id="welcome">KittyBlog</h1>
+            <button id="login">Login</button>
+            <button id="register">Register</button>
+        </div>
+    </div>
+    </>
+)
+}
+
+
+
