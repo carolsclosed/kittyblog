c6c9e4e4c97c8c12c889a55945f55391800abb0f
Carolina Sebastião <angelacarolina3456@gmail.com>
2024-12-09 14:24:59 +0000

    create folder of feed

--- frontoffice/KITTYBLOG/src/App.jsx
+++ frontoffice/KITTYBLOG/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css'
 import Navbar from './NavBar/Navbar'
 import Login from './Login/Login'
+import Feed from './feed/Feed'
 function App() {
 
   return (
     <>
-     <Login/>
+     <Feed/>
     </>
   )
 }
---
+++ frontoffice/KITTYBLOG/src/feed/Feed.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./Feed.css"
+
+function Feed(){
+    <>
+        
+    </>
+}
+
+export default Feed
